c7c663ea2ef9a9682c8a0d2156e0bb4a47298c5b
Max Herich <max.herich@gmail.com>
2026-01-11 22:17:00 +0100

    Úprava třídy LigaRepository

--- src/liga.py
+++ src/liga.py
@@ -16,24 +16,23 @@ class LigaRepository:
     def pridat(self, nazev, zeme, uroven):
         sql = f"INSERT INTO liga (nazev, zeme, uroven) VALUES (%s, %s, %s)"
         values = (nazev, zeme, uroven)
-        cursor.execute(sql, values)
+        cursor.execute(sql, values,)
         conn.connection.commit()
 
     def smazat(self, nazev):
         sql = f"DELETE FROM liga WHERE nazev = %s"
-        values = (nazev)
-        cursor.execute(sql, values)
+        values = (nazev,)
+        cursor.execute(sql, values,)
         conn.connection.commit()
 
-    def vyhledat(self, nazev):
-        sql = f"SELECT * FROM liga WHERE nazev = %s"
-        values = (nazev)
-        cursor.execute(sql, values)
-        conn.connection.commit()
-        return cursor.fetch()
+    def seznam_lig(self):
+        sql = f"SELECT * FROM liga"
+        cursor.execute(sql)
+        return cursor.fetchall()
 
-    def import_z_csv(self):
-        with open("csv_data/liga.csv", "r", encoding='utf-8') as file:
+    def import_z_csv(self, cesta_k_souboru):
+        with open(cesta_k_souboru, "r", encoding='utf-8') as file:
             reader = csv.reader(file)
+            next(reader, None)
             for row in reader:
-                self.pridat(row[1], row[2], row[3])
+                self.pridat(row[0], row[1], row[2])
