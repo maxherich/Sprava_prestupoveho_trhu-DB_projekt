d480abf582efa728f77e14220d47d9829bdc03f2
Max Herich <max.herich@gmail.com>
2026-01-11 16:56:50 +0100

    Úprava SQL skriptu

--- sql_skript/skript.sql
+++ sql_skript/skript.sql
@@ -1,3 +1,21 @@
+-- vytvoreni tabulky majitel
+create table majitel(
+	id int primary key auto_increment,
+    jmeno varchar(30) not null,
+    prijmeni varchar(30) not null,
+    email varchar(30) not null,
+    rozpocet float not null check (rozpocet > 0),
+    aktivni bool not null
+);
+-- vytvoreni tabulky liga
+create table liga(
+	id int primary key auto_increment,
+    nazev varchar(30) not null,
+    zeme varchar(30) not null,
+    uroven enum('1. liga', '2. liga', '3.liga') not null
+);
+
+-- vytvoreni tabulky klub
 create table klub(
 	id int primary key auto_increment,
     nazev varchar(30) not null,
@@ -7,6 +25,7 @@ create table klub(
     foreign key(majitel_id) references majitel(id)
 );
 
+-- vytvoreni tabulky hrac
 create table hrac(
 	id int primary key auto_increment,
     jmeno varchar(30) not null,
@@ -17,59 +36,38 @@ create table hrac(
     foreign key (klub_id) references klub(id)
 );
 
+-- vytvoreni tabulky prestup
 create table prestup(
 	id int primary key auto_increment,
     hrac_id int not null,
     foreign key(hrac_id) references hrac(id),
-    prodavajici_klub_id int not null,
-    foreign key(prodavajici_klub_id) references klub(id),
     kupujici_klub_id int not null,
-    foreign key(prodavajici_klub_id) references klub(id),
+    foreign key(kupujici_klub_id) references klub(id),
     datum date not null,
-    cena float not null,
-    check(prodavajici_klub_id != kupujici_klub_id)
+    cena float not null
 );
-
-create table majitel(
-	id int primary key auto_increment,
-    jmeno varchar(30) not null,
-    prijmeni varchar(30) not null,
-    email varchar(30) not null,
-    rozpocet float not null check (rozpocet > 0),
-    aktivni bool not null
-);
-
-create table liga(
-	id int primary key auto_increment,
-    nazev varchar(30) not null,
-    zeme varchar(30) not null,
-    uroven enum('1. liga', '2. liga', '3.liga') not null
-);
-
-LOAD DATA LOCAL INFILE 'D:\max\SKOLA\PV\PV-DB-projekt_fotbalovy_trh\csv_data\hrac.csv' INTO TABLE hrac FIELDS TERMINATED BY ',' IGNORE 1 ROWS;
-SHOW VARIABLES LIKE 'local_infile';
-SET GLOBAL local_infile = 1;
-
-insert into prestup(hrac_id, prodavajici_klub_id, kupujici_klub_id, datum, cena) values (1, 1, 2, sysdate(), 100000);
 
 delimiter //
-create trigger dokonceni_nakupu_hrace
-after insert
-on prestup
-for each row
-begin	
-	call nakup_hrace(new.kupujici_klub_id, new.prodavajici_klub_id, new.hrac_id);
-end//
-delimiter ;
-
-delimiter //
-create procedure nakup_hrace (kupujici_klub_id int, prodavajici_klub_id int, hrac_id int)
+create procedure nakup_hrace (kupujici_klub_id int, hrac_id int, cena int)
 begin	
 	start transaction;
-    update hrac set klub_id = kupujici_klub_id where id = new.hrac_id;
-    update majitel set rozpocet = rozpocet - cena where id = (select majitel_id from klub where id = kupujici_klub_id);
-    update majitel set rozpocet = rozpocet + cena where id = (select majitel_id from klub where id = prodavajici_klub_id);
+    update hrac set hrac.klub_id = kupujici_klub_id where hrac.id = hrac_id;
+    update majitel set rozpocet = (rozpocet-cena) where majitel.id = (select majitel_id from klub where klub.id = kupujici_klub_id);
+    update majitel set rozpocet = (rozpocet+cena) where majitel.id = (select majitel_id from klub where klub.id = (select klub_id from hrac where hrac.id = hrac_id));
     commit;
 end//
 delimiter ;
 
+create view hraci_tymu
+as
+select jmeno, prijmeni, cislo_dresu, pozice, klub.nazev
+from hrac
+inner join klub on hrac.klub_id = klub.id
+order by hrac.klub_id, cislo_dresu;
+
+create view seznam_aktivnich_majitelu
+as
+select jmeno, prijmeni, email, rozpocet, klub.nazev
+from majitel
+inner join klub on klub.majitel_id = majitel.id
+order by rozpocet desc;
